5270cc57c19983cb81aa53fcf93f575bc2ca2292
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-08 09:59:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+            do
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Sexo: ");
+                sexo = Console.ReadKey().KeyChar;
+
+            }
+            while (sexo != 'm' && sexo != 'f' && sexo != 'M' && sexo != 'F');
+            Console.WriteLine("\nOK");
+        }
+    }
+}
